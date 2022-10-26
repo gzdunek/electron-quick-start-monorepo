fdc73f8073717e34b2c2888f968e25da9560b03c
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2022-10-26 11:05:38 +0200

    Add `electron-builder` and native dependency (`node-pty`)

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const nodePty = require('node-pty');
 
 function createWindow () {
   // Create the browser window.
